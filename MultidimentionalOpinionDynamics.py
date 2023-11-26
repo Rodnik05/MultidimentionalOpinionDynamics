3d2bfb1464a362bc525416a18d5cdcce1643d674
nikita <nrodetckii@mail.ru>
2023-11-26 20:35:01 +0300

    first version without plots

---
+++ MultidimentionalOpinionDynamics.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class Dynamics:
+    def __init__(self,
+                 StartingOpinionMatrix : np.matrix,
+                 GramMatrix : np.matrix,
+                 t : int):
+        self.Opinions = []
+        self.Opinions.append(StartingOpinionMatrix)
+        self.GramMatrix = GramMatrix
+        self.t = t
+        self.Dist = self.dist(StartingOpinionMatrix, StartingOpinionMatrix, GramMatrix)
+        for i in range(t + 1):
+            self.ComputeNext()
+        
+ 
+    def AgentsCount(self):   
+        return np.shape(self.Opinions[0])[0]
+    
+
+    def ComputeNext(self):
+        NextOpinionMatrix = self.Opinions[-1]
+        for i in range(self.AgentsCount()):
+            DifSum = 0
+            for j in range(self.AgentsCount()):
+                DifSum += (self.Opinions[-1][j] - self.Opinions[-1][i]) * 1/(1 + self.Dist[j, i])
+                
+            DifSum /= self.AgentsCount()
+            NextOpinionMatrix[i] += DifSum
+            
+        self.Opinions.append(NextOpinionMatrix)
+        return NextOpinionMatrix
+    
+
+    def dist(self, A, B, GramMatrix):
+        return np.sqrt(
+            np.diag(np.matmul(
+                np.matmul(A, GramMatrix), 
+                A.T)).reshape(-1, 1) -
+            2 * (np.matmul(
+                np.matmul(A, GramMatrix), 
+                B.T)) +
+            np.diag(np.matmul(
+                np.matmul(B, GramMatrix), 
+                B.T)))    
---
+++ main.py
@@ -0,0 +1,10 @@
+from MultidimentionalOpinionDynamics import Dynamics
+import numpy as np
+
+
+a = Dynamics(np.matrix([[-1, 0],[1, 0], [0, 4], [0, -1]], dtype=np.float64),
+             np.matrix([[1, 0], [0, 1]], dtype=np.float64),
+             500)
+
+for opinion in a.Opinions:
+    print(opinion)
